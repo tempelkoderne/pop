8a664a63f5c9d0b64398f67132eea79f36adf0e4
Anders Geil <anders.geil@gmail.com>
2016-12-20 15:54:55 +0100

    fixed naming conventions

--- 10g/10g.fsx
+++ 10g/10g.fsx
@@ -1,22 +1,22 @@
 // 10g.0
 
 type Animal (weight:float, speedMax:float) =
-    let mutable currentSpeed = 0.0
-    let rand = new System.Random()
+    let mutable SpeedCurrent = 0.0
+    let rand = System.Random()
     
     member self.Food = (float (rand.Next(0,101))) / 100.0
     member self.Weight = weight
     member self.SpeedMax = speedMax
     member self.Speed
-        with get() = currentSpeed
+        with get() = SpeedCurrent
 
     member self.FoodNeed() = weight/2.0
-    member self.SetSpeed() = (currentSpeed <- self.SpeedMax * (self.Food * self.FoodNeed()))
+    member self.SpeedSet() = (SpeedCurrent <- self.SpeedMax * (self.Food * self.FoodNeed()))
 
-    new (speed_max:float) =
+    new (speedMax:float) =
         let rand = System.Random()
         let rweight = float (rand.Next(70,301))
-        Animal(rweight, speed_max)
+        Animal(rweight, speedMax)
 
 
 type Carnivore (weight:float, speedMax:float) =
@@ -33,6 +33,3 @@ let cheetah = Carnivore (50.0, 114.0)
 let antelope = Herbivore (50.0, 95.0)
 let wildebeest = Herbivore (200.0, 80.0)
 
-food_actual = random % of self.FoodMax
-food_equilibrium() = weight/2
-speed() = speedMax * food_actual/food_equilibrium 
