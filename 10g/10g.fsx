14264325dafe10d59cfd798e7b8c8beb06caccbf
Satannius <TOS@Tobiass-MacBook-Pro.local>
2016-12-22 14:45:31 +0100

    Dokumenteret.

--- 10g/10g.fsx
+++ 10g/10g.fsx
@@ -1,26 +1,59 @@
-// 10g.0
+(*Animal*)
+///<summary>
+/// A class object.
+///</summary>
+///<params name="weight">
+/// A float.
+///</params>
+///<params name="speedMax">
+/// A float.
+///</params>
+///<returns>
+/// An instance of the Animal object with the assigned weight and speedMax,
+/// or a randomly generated weight and assigned speedMax.
+///</returns>
+///<remarks>
+/// An additional constructor allows an instance to be created with only one
+/// supplied parameter-value to speedMax.
+///</remarks>
 
 type Animal =
+    // Constructor parameters.
     val weight : float
     val speedMax : float
     val mutable speed : float
+    // Constructor.
     new (weight, speedMax) = {
         weight = max 0.0 weight
         speedMax = max 0.0 speedMax
         speed = 0.0
         }
+    // Additional constructor.
     new (speedMax:float) = {
         weight = float (System.Random().Next(70,301))
         speedMax = max 0.0 speedMax
         speed = 0.0
         }
-    
+    // Attribute. Generates an amount of Food based on a random percentage of FoodNeed.
     member self.Food = self.FoodNeed() * (float (System.Random().Next(0, 101)))/100.0 
-
+    // Methods for setting FoodNeeded and speed.
     member self.FoodNeed() = self.weight/2.0
     member self.Run() = (self.speed <- self.speedMax * self.Food/100.0)
 
-
+(*Carnivore*)
+///<summary>
+/// A class object. Inherits Animal.
+///</summary>
+///<params name="weight">
+/// A float. (Via Animal)
+///</params>
+///<params name="speedMax">
+/// A float. (Via Animal)
+///</params>
+///<returns>
+/// An instance of the Carnivore object with the assigned weight and speedMax,
+/// or a randomly generated weight and assigned speedMax.
+///</returns>
 type Carnivore =
     inherit Animal
     
@@ -28,7 +61,20 @@ type Carnivore =
     new (speedMax) = {inherit Animal (speedMax)}    
     member self.Food = self.weight * 0.08
 
-
+(*Herbivore*)
+///<summary>
+/// A class object. Inherits Animal.
+///</summary>
+///<params name="weight">
+/// A float. (Via Animal)
+///</params>
+///<params name="speedMax">
+/// A float. (Via Animal)
+///</params>
+///<returns>
+/// An instance of the Herbivore object with the assigned weight and speedMax,
+/// or a randomly generated weight and assigned speedMax.
+///</returns>
 type Herbivore =
     inherit Animal
     
