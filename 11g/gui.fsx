3fdddf25fba44b8b63dc3240cc84a307faa987a0
Anders Geil <anders.geil@gmail.com>
2017-01-11 22:47:53 +0100

    working static framework

--- 11g/gui.fsx
+++ 11g/gui.fsx
@@ -3,33 +3,22 @@ open System.Drawing
 // open Graph
 
 // define types
-type pen = Color * float
-type point = (float * float)
-type polyline = point list
-
-// set helpers
-let addPoints p0 p1 : point =
-  (fst p0 + fst p1, snd p0 + snd p1)
-let rotatePoint theta p : point =
-  ((fst p) * cos theta - (snd p) * sin theta, (fst p) * sin theta + (snd p) * cos theta)
-let rotatePoints theta l : polyline =
-  List.map (rotatePoint theta) l
-let scalePoint s p : point =
-  (s * fst p, s * snd p)
-let translatePoints d l : polyline =
-  List.map (addPoints d) l
+// type point = (int * int)
+type planet = (Color * int)
+// type planets = planet * point list
+// type polyline = Color * int list
 
 
 (***** BUILD SOLAR SYSTEM *****)
 
 // create planet
-let createPlanet backgroundColor (width, height) title draw =
-  let win = new Form ()
-  win.Text <- title
-  win.BackColor <- backgroundColor
-  win.ClientSize <- Size (width, height)
-  win.Paint.Add draw
-  win
+let bigBang backgroundColor (width, height) title draw =
+    let win = new Form ()
+    win.Text <- title
+    win.BackColor <- backgroundColor
+    win.ClientSize <- Size (width, height)
+    win.Paint.Add draw
+    win
 
 // draw planet
 (*
@@ -42,26 +31,25 @@ let drawPlanet (coords : polyline byref) (pen : pen) (e : PaintEventArgs) =
   e.Graphics.DrawLines (Pen, Points)
 *)
 
-let drawPlanet (coords : float*float []) (pen : pen) (e : PaintEventArgs) =
-  let pairToPoint p = Point (int (round (fst p)), int (round (snd p)))
-  let color, width = pen
-  let Pen = new Pen (color, single width)
-  let Points = Array.map pairToPoint (coords)
-  
-  let shape = new Rectangle(Point(fst coords, snd coords),
-                            Size(20, 20))
-  e.Graphics.FillEllipse (Pen, Points, shape)
+let drawPlanet (coord : int*int) (pl : planet) (e : PaintEventArgs) =
+    let x, y = coord
+    let color, radius = pl
+    let brush = new SolidBrush (color)
+    let shape = new Rectangle(Point(x, y),
+                              Size(radius, radius))
+    e.Graphics.FillEllipse (brush, shape)
+
+// brief tests without movement
+let mutable pos1 = (100,100)
+let earth = (Color.Blue, 30)
+let world = bigBang Color.Black (400,400) "Solar System" (drawPlanet pos1 earth)
+Application.Run world
+
+///////// TODO: add movement using Timer-module
 
 
-// position updates
-let updatePlanet (planet : Form) (coords : int*int) dtheta center showtime =
-  let centeredCoords = translatePoints (scalePoint -1.0 center) coords
-  let rotatedCoords = rotatePoints dtheta centeredCoords
-  coords <- translatePoints center rotatedCoords
-  planet.Refresh ()
-
-
-// setup solar system details
+(*
+// solar system config
 let title = "Solar system"
 let backgroundColor = Color.Black
 let size = (400, 400)
@@ -70,16 +58,21 @@ let mutable coords = List.map (polarToCartesian 100.0) [0.0;
                                                         2.0/3.0*System.Math.PI;
                                                         4.0/3.0*System.Math.PI;
                                                         0.0]
+
 let center = scalePoint (1.0/2.0) (float (fst size), float (snd size))
 coords <- translatePoints center coords
-let pen = (Color.Red, 1.0)
+let earth = (Color.Red, 10)
 
-// let col = Color.Red
-// let rad = 10
-
-let earth = createPlanet backgroundColor size title (drawPlanet &coords pen) 
+let world = bigBang col size title (drawPlanet pos earth)
+*)
 
 
+// position updates
+let updatePlanet (planet : Form) (coords : int*int list byref) (newCoords : int*int) showtime =
+    // let centeredCoords = translatePoints (scalePoint -1.0 center) coords
+    // let rotatedCoords = rotatePoints dtheta centeredCoords
+    coords <- newCoords
+    planet.Refresh ()
 
 // setup timer
 let timer = new Timer()
@@ -88,4 +81,4 @@ timer.Enabled <- true
 let dtheta = 0.01
 timer.Tick.Add (updatePlanet earth &coords dtheta center)
 
-Application.Run earth
+Application.Run world
