90fa2349429ba9b0bcb9aa62b902d8df00fc2374
Anders Geil <anders.geil@gmail.com>
2017-01-12 17:05:03 +0100

    unassorted fiddlin'

--- 11g/gui.fsx
+++ 11g/gui.fsx
@@ -1,14 +1,10 @@
 open System.Windows.Forms
 open System.Drawing
 
-// define types
 type coord = (int * int)
 type path = coord list
 type planet = (Color * int)
-// type planets = planet * point list
-
-
-(***** BUILD SOLAR SYSTEM *****)
+// type planets = planet * path list
 
 // create world
 let bigBang backColor (width, height) title draw =
@@ -20,8 +16,8 @@ let bigBang backColor (width, height) title draw =
     win
 
 // draw planets
-let drawPlanet (pos:coord byref) (pl:planet) (e:PaintEventArgs) =
-    let x, y = pos
+let drawPlanet (pos:path byref) (pl:planet) (e:PaintEventArgs) =
+    let x, y = pos.[0]
     let color, radius = pl
     let brush = new SolidBrush (color)
     let shape = new Rectangle(Point(x, y),
@@ -29,19 +25,28 @@ let drawPlanet (pos:coord byref) (pl:planet) (e:PaintEventArgs) =
     e.Graphics.FillEllipse (brush, shape)
 
 // god config
-let mutable pos1:coord = (100,100)
+let mutable pos0:path = [(100,100);
+                          (200,100);
+                          (200,200);
+                          (100,200);
+                          (100,100)]
 let earth:planet = (Color.Blue, 30)
-let world = bigBang Color.Black (400,400) "Solar System" (drawPlanet &pos1 earth)
+let size:int*int = (400,400)
+let bcolor:Color = Color.Black
+let title:string = "Solar System"
+let solar = bigBang bcolor size title (drawPlanet &pos0 earth)
 
 // position updates
-let updateWorld (world:Form) (pos:coord byref) showtime =
-    pos <- (1 + (fst pos), 1 + (snd pos))
+let updateWorld (world:Form) (pos:path byref) showtime =
+    pos <- [(1 + (fst (pos.[0])), (1 + snd (pos.[0])))]
+    // pos <- calcPos(iniCoor 1.0) // calcPos under construction
     world.Refresh()
 
 // setup timer
-let timer = new Timer()
-timer.Interval <- 100 // milliseconds between ticks
-timer.Enabled <- true // timer is now running    
-timer.Tick.Add (updateWorld world &pos1)
+let timer = new Timer(Interval = 100,
+                      Enabled = true)
+// timer.Tick.Add (fun showtime -> updateWorld solar &pos1)                
+timer.Tick.Add (updateWorld solar &pos0)
 
-Application.Run world
+// run system
+Application.Run solar
