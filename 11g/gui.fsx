53fb0449960dcc07ce64882aca461cb499d4a2bf
Anders Geil <anders.geil@gmail.com>
2017-01-13 16:41:59 +0100

    working animation, but odd trails

--- 11g/gui.fsx
+++ 11g/gui.fsx
@@ -10,24 +10,34 @@ type planet = (Color * int)
 let toPx (x:coord) = (log10((fst x) + 100.0) * 100.0, log10((snd x) + 100.0) * 100.0)
 let ofPx (x:coord) = (10.0**((fst x)/100.0) - 100.0, 10.0**((fst x)/100.0) - 100.0)
 
-let scaleUp (x:coord) = (100.0 * (fst x), 100.0 * (snd x))
-let scaleDown (x:coord) = ((fst x)/100.0, (fst x)/100.0)
+let scaleUp (x:coord) = (1000.0 * (fst x), 1000.0 * (snd x))
+let scaleDown (x:coord) = ((fst x)/1000.0, (snd x)/1000.0)
 
 let offset (r:int) (x:coord) = ((float r) + (fst x), (float r) + (snd x))
+
+let ( .+ ) x y = ((fst x + fst y),(snd x + snd y))
+let ( .* ) x y = (x*(fst y),x*(snd y))
+
+let mutable day = 0
 
 // compute next position
 let CNPP (data : coord []) =
     let GMs = 2.959122082322128e-4
-    let ( .+ ) x y = ((fst x + fst y),(snd x + snd y))
-    let ( .* ) x y = (x*(fst y),x*(snd y))
     let r0 = data.[0]
     let v0 = data.[1]
     let a0 = data.[2]
+    day <- day + 1
+    printfn "DAY NUMBER %A" day
     let r1 = (r0 .+ v0)
+    printfn "r1: %A" r1
     let r0Len = sqrt (((fst r0)**2.0) + ((snd r0)**2.0))
+    printfn "r0len: %A" r0Len
     let a1 = -(GMs/(r0Len**3.0)) .* r0
+    printfn "a1: %A" a1
     let v1 = (v0 .+ a0)
+    printfn "v1: %A" v1
     let positions = [|r1;v1;a1|]
+    printfn "positions: %A" positions
     positions
 
 // create world
@@ -50,7 +60,7 @@ let drawPlanet (pos:coord byref) (pl:planet) (e:PaintEventArgs) =
     e.Graphics.FillEllipse (brush, shape)
 
 // god config
-let mutable pos0 = scaleUp (-0.1666759033, 0.969084551)
+let mutable p0 = scaleUp (-0.1666759033, 0.969084551)
 let mutable v0 = scaleUp (-0.01720978776, -0.003125375992)
 let mutable a0 = scaleUp (5.187516652e-05, -0.0003016118194)
 
@@ -58,22 +68,23 @@ let earth:planet = (Color.Blue, 50)
 let size:(int*int) = (700,700)
 let bcolor:Color = Color.Black
 let title:string = "Solar System"
-let solar = bigBang bcolor size title (drawPlanet &pos0 earth)
+let solar = bigBang bcolor size title (drawPlanet &p0 earth)
 
 // move planets
-let updateWorld (world:Form) (pos:coord byref) (v:coord byref) (a:coord byref) showtime =
-    let state = CNPP [|scaleDown pos; scaleDown v; scaleDown a|]
-    pos <- scaleUp state.[0]
+let updateWorld (world:Form) (p:coord byref) (v:coord byref) (a:coord byref) showtime =
+    let state = CNPP [|scaleDown p; scaleDown v; scaleDown a|]
+    p <- scaleUp state.[0]
     v <- scaleUp state.[1]
     a <- scaleUp state.[2]
-    printfn "%A" state
+    printfn "SCALED STATE: %A" state
+    // printfn "%A" state
     world.Refresh()
 
 
 // setup timer
 let timer = new Timer(Interval = 10,
                       Enabled = true)
-timer.Tick.Add (updateWorld solar &pos0 &v0 &a0)
+timer.Tick.Add (updateWorld solar &p0 &v0 &a0)
 
 // run system
 Application.Run solar
